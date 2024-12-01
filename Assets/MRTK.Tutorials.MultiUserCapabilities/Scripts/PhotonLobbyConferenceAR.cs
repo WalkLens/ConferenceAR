68fdb0585c8f14fbfe2501a26e7ec14a5975d6f5
aloHyomora <alohomoratolit@naver.com>
2024-12-01 17:42:09 +0900

    [Fix] : 다른 로비, 룸에 입장하는 오류

--- Assets/MRTK.Tutorials.MultiUserCapabilities/Scripts/PhotonLobbyConferenceAR.cs
+++ Assets/MRTK.Tutorials.MultiUserCapabilities/Scripts/PhotonLobbyConferenceAR.cs
@@ -1,6 +1,7 @@
 ﻿using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine;
+using CustomLogger;
 
 namespace MRTK.Tutorials.MultiUserCapabilities
 {
@@ -13,6 +14,11 @@ namespace MRTK.Tutorials.MultiUserCapabilities
 
         private void Awake()
         {
+            Debug.Log(Application.persistentDataPath);
+            #if !UNITY_EDITOR
+                FileLogger.ClearLog();  
+            #endif
+
             if (Lobby == null)
             {
                 Lobby = this;
@@ -39,28 +45,54 @@ namespace MRTK.Tutorials.MultiUserCapabilities
             PhotonNetwork.AuthValues.UserId = randomUserId.ToString();
             userIdCount++;
             PhotonNetwork.NickName = PhotonNetwork.AuthValues.UserId;
-            PhotonNetwork.JoinRandomRoom();
+
+            // 먼저 로비에 입장
+            TypedLobby conferenceLobby = new TypedLobby("Conference", LobbyType.Default);
+            PhotonNetwork.JoinLobby(conferenceLobby);   
+            
+
+            FileLogger.Log($"마스터 서버 {conferenceLobby.Name} 연결 완료, 로비 입장 시도", this);            
+        }
+
+        public override void OnJoinedLobby()
+        {            
+            FileLogger.Log("로비 입장 완료", this);
+            
+            // 마스터 클라이언트만 방을 생성하도록 함
+            if (PhotonNetwork.IsMasterClient)
+            {
+                var roomOptions = new RoomOptions {IsVisible = true, IsOpen = true, MaxPlayers = 10};
+                PhotonNetwork.CreateRoom("DefaultRoom", roomOptions);
+            }
+            else
+            {
+                // 마스터가 아닌 클라이언트는 방 입장만 시도
+                PhotonNetwork.JoinRoom("DefaultRoom");
+            }
+        }
+        public override void OnLeftLobby(){
+            Debug.Log("로비 퇴장");
         }
 
         public override void OnJoinedRoom()
         {
-            base.OnJoinedRoom();
-
-            Debug.Log("\nPhotonLobby.OnJoinedRoom()");
-            Debug.Log("Current room name: " + PhotonNetwork.CurrentRoom.Name);
-            Debug.Log("Other players in room: " + PhotonNetwork.CountOfPlayersInRooms);
-            Debug.Log("Total players in room: " + (PhotonNetwork.CountOfPlayersInRooms + 1));
+            base.OnJoinedRoom();            
+            // For Debugging
+            FileLogger.Log("PhotonLobbyConferenceAR.OnJoinedRoom()", this);
+            FileLogger.Log("Current room name: " + PhotonNetwork.CurrentRoom.Name, this);
+            FileLogger.Log("Other players in room: " + PhotonNetwork.CountOfPlayersInRooms, this);
+            FileLogger.Log("Total players in room: " + (PhotonNetwork.CountOfPlayersInRooms + 1), this);
+            
         }
-
-        public override void OnJoinRandomFailed(short returnCode, string message)
+        public override void OnJoinRoomFailed(short returnCode, string message)
         {
+            FileLogger.Log("방 참가 실패 - 방이 존재하지 않아 새로 생성합니다.", this);
             CreateRoom();
-        }
+        }        
 
         public override void OnCreateRoomFailed(short returnCode, string message)
         {
-            Debug.Log("\nPhotonLobby.OnCreateRoomFailed()");
-            Debug.LogError("Creating Room Failed");
+            FileLogger.Log("방 생성 실패: " + message, this);
             CreateRoom();
         }
 
@@ -68,6 +100,8 @@ namespace MRTK.Tutorials.MultiUserCapabilities
         {
             base.OnCreatedRoom();
             roomNumber++;
+            FileLogger.Log("방 생성 완료: " + PhotonNetwork.CurrentRoom.Name, this);
+            FileLogger.Log("방 번호: " + roomNumber, this);
         }
 
         public void OnCancelButtonClicked()
@@ -80,11 +114,14 @@ namespace MRTK.Tutorials.MultiUserCapabilities
             PhotonNetwork.ConnectUsingSettings();
             Lobby = this;
         }
-
+        
         private void CreateRoom()
         {
             var roomOptions = new RoomOptions {IsVisible = true, IsOpen = true, MaxPlayers = 10};
-            PhotonNetwork.CreateRoom("Room" + Random.Range(1, 3000), roomOptions);
+
+            // For Debugging
+            // TODO: User Matching Manager에서 관리하는 이름으로 방 생성
+            PhotonNetwork.CreateRoom("DefaultRoom", roomOptions);
         }
     }
 }
